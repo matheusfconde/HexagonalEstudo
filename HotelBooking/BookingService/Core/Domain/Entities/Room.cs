e0c5ac6cf58642063640964ed1d08c481425ca92
matheusfconde <matheusf.conde@gmail.com>
2022-09-13 10:38:00 -0300

    Escrevendo Unit Tests State Machine

---
+++ HotelBooking/BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Level { get; set; }
+        public bool InMaintenance { get; set; }
+        public bool IsAvailable
+        {
+            get
+            {
+                if (this.InMaintenance || !this.HasGuest)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        public bool HasGuest
+        {
+            //verificar se existem Bookings abertos para esta Room
+            get { return true; }
+        }
+    }
+}
