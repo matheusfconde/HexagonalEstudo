df5e582c172db73242c9198ac0d3ea662ee8a6a1
matheusfconde <matheusf.conde@gmail.com>
2022-09-13 15:34:03 -0300

    Criando as tabelas no banco de dados via Migrations

--- HotelBooking/BookingService/Core/Domain/Entities/Booking.cs
+++ HotelBooking/BookingService/Core/Domain/Entities/Booking.cs
@@ -13,6 +13,9 @@ namespace Domain.Entities
         public DateTime PlaceAt { get; set; }
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+        public Room Room { get; set; }
+        public Guest Guest { get; set; }
+
         private Status Status { get; set; }
 
         public Status CurrentStatus { get { return this.Status; } }
