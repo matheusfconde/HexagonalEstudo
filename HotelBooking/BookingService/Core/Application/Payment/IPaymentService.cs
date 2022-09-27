8c540eb503ddd2ded9bd9d44fd2beb62d84ed7a6
matheusfconde <matheusf.conde@gmail.com>
2022-09-27 11:08:55 -0300

    adição certificado

---
+++ HotelBooking/BookingService/Core/Application/Payment/IPaymentService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Payment.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Payment
+{
+    public interface IPaymentService
+    {
+        Task<PaymentStateDto> PayWithCreditCard(string paymentIntention);
+        Task<PaymentStateDto> PayWithDebitCard(string paymentIntention);
+        Task<PaymentStateDto> PayBankTransfer(string paymentIntention);
+    }
+}
